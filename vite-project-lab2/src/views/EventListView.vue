5bf3b9c3d62ced01bf877380b7742fb0d4917f7d
Baibua <siriyakorn14869@gmail.com>
2023-08-11 10:00:17 +0700

    2.9

--- vite-project-lab2/src/views/EventListView.vue
+++ vite-project-lab2/src/views/EventListView.vue
@@ -11,6 +11,7 @@ import type { Axios, AxiosResponse } from 'axios';
 const events: Ref<Array<EventItem>> = ref([])
 
 const totalEvent = ref<number>(0)
+const eventsPerPage = ref(2) //initial value of events
 const props = defineProps({
   page: {
     type: Number,
@@ -18,20 +19,22 @@ const props = defineProps({
   }
 })
 
-  EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
+  EventService.getEvent(eventsPerPage.value, props.page).then((response: AxiosResponse<EventItem[]>) => {
     events.value = response.data
 })
 
-  watchEffect(() => {
-    EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
+watchEffect(() => {
+  EventService.getEvent(eventsPerPage.value, props.page).then(
+    (response: AxiosResponse<EventItem[]>) => {
       events.value = response.data
       totalEvent.value = response.headers['x-total-count']
-    })
+    }
+  )
 })
 
 const hasNextPage = computed(() => {
   // first calculate the total page 
-  const totalPages = Math.ceil(totalEvent.value / 2)
+  const totalPages = Math.ceil(totalEvent.value / eventsPerPage.value)
   return props.page.valueOf() < totalPages
 })
   
@@ -40,13 +43,21 @@ const hasNextPage = computed(() => {
 <template>
     <h1>Events For Good</h1>
   <main class="events">
+    <div class="events-input">
+      <label for="events-per-page">Events per page:</label>
+      <input type="number" id="events-per-page" v-model.number="eventsPerPage" />
+    </div>
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+    <div class="pagination">
+    
       <RouterLink :to="{ name: 'EventList', query: {page: page - 1} }" rel="prev" v-if="page!= 1">
         Prev Page
       </RouterLink>
       <RouterLink :to=" { name: 'EventList', query: { page: page +1 } }" rel="next" v-if="hasNextPage">
         Next Page
       </RouterLink>
+
+    </div>
   </main>
 </template>
 
@@ -55,5 +66,21 @@ const hasNextPage = computed(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: blue;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
