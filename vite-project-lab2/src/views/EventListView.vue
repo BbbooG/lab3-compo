5fb3f4ccc1f9da5f400a5fb9ee988fe494f2821e
Baibua <siriyakorn14869@gmail.com>
2023-08-11 09:47:30 +0700

    2.1-2.7

--- vite-project-lab2/src/views/EventListView.vue
+++ vite-project-lab2/src/views/EventListView.vue
@@ -4,13 +4,35 @@ import EventOrganizer from '../components/EventOrganizer.vue'
 
 import  type { EventItem } from '@/type';
 
+import { computed, ref, watchEffect } from 'vue'
 
-import { ref, Ref } from 'vue'
 import EventService from '@/services/EventService'
+import type { Axios, AxiosResponse } from 'axios';
 const events: Ref<Array<EventItem>> = ref([])
 
-EventService.getEvent().then((response) => {
-events.value = response.data
+const totalEvent = ref<number>(0)
+const props = defineProps({
+  page: {
+    type: Number,
+    required: true
+  }
+})
+
+  EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
+    events.value = response.data
+})
+
+  watchEffect(() => {
+    EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
+      events.value = response.data
+      totalEvent.value = response.headers['x-total-count']
+    })
+})
+
+const hasNextPage = computed(() => {
+  // first calculate the total page 
+  const totalPages = Math.ceil(totalEvent.value / 2)
+  return props.page.valueOf() < totalPages
 })
   
 </script>
@@ -19,7 +41,12 @@ events.value = response.data
     <h1>Events For Good</h1>
   <main class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
-   
+      <RouterLink :to="{ name: 'EventList', query: {page: page - 1} }" rel="prev" v-if="page!= 1">
+        Prev Page
+      </RouterLink>
+      <RouterLink :to=" { name: 'EventList', query: { page: page +1 } }" rel="next" v-if="hasNextPage">
+        Next Page
+      </RouterLink>
   </main>
 </template>
 
